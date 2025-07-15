4c2ee17398fe226347bc8823a38dbabe0e538598
Aigerim Zhaiymbet <s_zhaiymbet24@stud.hwr-berlin.de>
2025-07-15 18:30:43 +0200

    reducer changed

--- reducer.py
+++ reducer.py
@@ -1,3 +1,23 @@
+# #!/usr/bin/env python
+# import sys
+
+# current_category = None
+# count = 0
+
+# for line in sys.stdin:
+#     category, sales = line.strip().split('\t')
+    
+#     if current_category == category:
+#         count += 1
+#     else:
+#         if current_category:
+#             print(f"{current_category}\t{count}")
+#         current_category = category
+#         count = 1
+
+# if current_category:
+#     print(f"{current_category}\t{count}")
+
 #!/usr/bin/env python
 import sys
 
@@ -10,10 +30,10 @@ for line in sys.stdin:
     if current_category == category:
         count += 1
     else:
-        if current_category:
+        if current_category and count > 114:
             print(f"{current_category}\t{count}")
         current_category = category
         count = 1
 
-if current_category:
+if current_category and count > 114:
     print(f"{current_category}\t{count}")
