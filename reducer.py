d6362121675b18924259a433cf4b20d44be438da
Aigerim Zhaiymbet <s_zhaiymbet24@stud.hwr-berlin.de>
2025-07-15 18:31:28 +0200

    reducer changed 2

--- reducer.py
+++ reducer.py
@@ -19,21 +19,46 @@
 #     print(f"{current_category}\t{count}")
 
 #!/usr/bin/env python
+# import sys
+
+# current_category = None
+# count = 0
+
+# for line in sys.stdin:
+#     category, sales = line.strip().split('\t')
+    
+#     if current_category == category:
+#         count += 1
+#     else:
+#         if current_category and count > 114:
+#             print(f"{current_category}\t{count}")
+#         current_category = category
+#         count = 1
+
+# if current_category and count > 114:
+#     print(f"{current_category}\t{count}")
+
+#!/usr/bin/env python
 import sys
 
 current_category = None
+total_sales = 0
 count = 0
 
 for line in sys.stdin:
     category, sales = line.strip().split('\t')
     
     if current_category == category:
+        total_sales += float(sales)
         count += 1
     else:
-        if current_category and count > 114:
-            print(f"{current_category}\t{count}")
+        if current_category:
+            average = total_sales / count
+            print(f"{current_category}\t{average:.2f}")
         current_category = category
+        total_sales = float(sales)
         count = 1
 
-if current_category and count > 114:
-    print(f"{current_category}\t{count}")
+if current_category:
+    average = total_sales / count
+    print(f"{current_category}\t{average:.2f}")
