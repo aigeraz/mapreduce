e13e5612f08eb9fe1be4b69938e200801cb4e176
Aigerim Zhaiymbet <s_zhaiymbet24@stud.hwr-berlin.de>
2025-07-15 18:30:03 +0200

    mapper changed 2

--- mapper.py
+++ mapper.py
@@ -7,12 +7,24 @@
 #         date, time, item, category, sales, payment = data
 #         print(f"{category}\t{sales}")
 
+# #!/usr/bin/env python
+# import sys
+
+# for line in sys.stdin:
+#     data = line.strip().split('\t')
+#     if len(data) != 6:
+#         raise ValueError(f"Expected 6 fields, got {len(data)}: {line.strip()}")
+#     date, time, item, category, sales, payment = data
+#     print(f"{category}\t{sales}")
+
 #!/usr/bin/env python
 import sys
 
+allowed_categories = {"Computers", "Cameras", "Video Games"}
+
 for line in sys.stdin:
     data = line.strip().split('\t')
-    if len(data) != 6:
-        raise ValueError(f"Expected 6 fields, got {len(data)}: {line.strip()}")
-    date, time, item, category, sales, payment = data
-    print(f"{category}\t{sales}")
+    if len(data) == 6:
+        date, time, item, category, sales, payment = data
+        if category in allowed_categories:
+            print(f"{category}\t{sales}")
